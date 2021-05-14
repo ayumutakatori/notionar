66c8663ed35b1297e02b244521bb8b3f60f410a2
Katsumata Ryo <ktmt.ryo@gmail.com>
2021-05-14 11:43:24 +0900

    initialize

---
+++ spec/notionar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Notionar do
+  it "has a version number" do
+    expect(Notionar::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
