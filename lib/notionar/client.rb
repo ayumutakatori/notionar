262a3b4e411b559b3e01aadee5ffcd1471c1910f
Katsumata Ryo <ktmt.ryo@gmail.com>
2021-05-15 15:51:48 +0900

    first commit

---
+++ lib/notionar/client.rb
@@ -0,0 +1,78 @@
+require 'net/http'
+require 'json'
+
+module Notionar
+  class Client
+    def initialize(token)
+      endpoint = "https://api.notion.com/"
+      @version  = "v1"
+
+      url = URI.parse(endpoint)
+      @http = Net::HTTP.new(url.host, url.port)
+      @http.use_ssl = true
+      @headers = {
+        "Authorization" => "Bearer #{token}",
+        "Content-Type"  => "application/json",
+        "Notion-Version" =>  "2021-05-13",
+      }
+    end
+
+    def users
+      @path = "/#{@version}/users"
+      request(:get)
+    end
+
+    def user(user_id)
+      @path = "/#{@version}/users/#{user_id}"
+      request(:get)
+    end
+
+    def page(page_id)
+      @path = "/#{@version}/pages/#{page_id}"
+      request(:get)
+    end
+
+    def search(query)
+      @path = "/#{@version}/search"
+      @params = {
+        query: query,
+      }
+
+      request(:post)
+    end
+
+    def database(database_id)
+      @path = "/#{@version}/databases/#{database_id}"
+      request(:get)
+    end
+
+    def query_database(database_id, property_name, query)
+      @path = "/#{@version}/databases/#{database_id}/query"
+      @params = {
+        filter: {
+          property: property_name,
+          select: {
+            equals: query
+          }
+        }
+      }
+      request(:post)
+    end
+
+    private
+
+    def request(method)
+      response =
+        case method
+        when :get
+          @http.get(@path, @headers)
+        when :post
+          @http.post(@path, JSON.generate(@params), @headers)
+        else
+          raise "Unknown method - #{method}"
+        end
+
+      JSON.parse(response)
+    end
+  end
+end
